ec2f392c45d47d25eb48b230ae0b5ff38508a8ed
bridgesmith17 <bridgesmith17@gmail.com>
2016-05-30 10:56:57 -0700

    Update helloMysql.js

--- helloMysql.js
+++ helloMysql.js
@@ -23,7 +23,7 @@ app.all('/',function(req,res,next){
     } 
     context.results = rows;
     console.log(context);
-    console.log(context[0].name);
+    console.log(context[0].results[0]);
 
     
 /*    
