1d82acc9c847bffeab0c0353e5d71e42c89dfd4e
bridgesmith17 <bridgesmith17@gmail.com>
2016-05-29 18:07:50 -0700

    Update helloMysql.js

--- helloMysql.js
+++ helloMysql.js
@@ -24,7 +24,7 @@ app.get('/',function(req,res,next){
 
 app.get('/insert',function(req,res,next){
   var context = {};
-  mysql.pool.query("INSERT INTO todo (`name`, `done`) VALUES (?,?)", [req.query.c], function(err, result){
+  mysql.pool.query("INSERT INTO todo (`name`) VALUES (?)", [req.query.c], function(err, result){
     if(err){
       next(err);
       return;
