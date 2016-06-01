7e2c867a2ed51216bbcdc92d987cbae8288bf786
bridgesmith17 <bridgesmith17@gmail.com>
2016-06-01 14:52:23 -0700

    Update helloMysql.js

--- helloMysql.js
+++ helloMysql.js
@@ -53,6 +53,22 @@ app.post('/update',function(req,res,next){
 });
 });
 
+app.post('update-do',function(req,res, next){
+
+      mysql.pool.query("UPDATE workouts SET name=?, reps=?, weight=?, date=?, lbs=? WHERE id=? ",
+        [req.body.name, req.body.reps, req.body.weight, req.body.date, req.body.lbs, req.body.updateID],
+        function(err, result){
+        if(err){
+          next(err);
+          return;
+        }
+      
+      
+      });
+    
+      
+    });
+
 
 
 
