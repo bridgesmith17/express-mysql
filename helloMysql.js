29e1172a4b22e1bdec73a172657497a504e6ec79
bridgesmith17 <bridgesmith17@gmail.com>
2016-06-01 15:19:30 -0700

    Update helloMysql.js

--- helloMysql.js
+++ helloMysql.js
@@ -40,7 +40,7 @@ app.post('/delete', function(req, res, next){
 
 
 app.post('/update',function(req,res,next){
-  console.log(req.body.updateID);
+
   var context = {};
     mysql.pool.query("SELECT * FROM workouts WHERE id=?", [req.body.updateID], function(err, result){
     if(err){
@@ -55,6 +55,7 @@ app.post('/update',function(req,res,next){
 
 app.post('/update-do',function(req,res, next){
 console.log(req.body.name);
+  console.log(req.body.updateID);
   //    mysql.pool.query("UPDATE workouts SET name=?, reps=?, weight=?, date=?, lbs=? WHERE id=? ",
     //    [req.body.name, req.body.reps, req.body.weight, req.body.date, req.body.lbs, req.body.updateID],
       
