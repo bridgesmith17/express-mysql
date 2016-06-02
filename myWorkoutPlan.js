60859850bb82de78b96eb3ed36beca150a642c2d
bridgesmith17 <bridgesmith17@gmail.com>
2016-06-01 17:13:36 -0700

    Update myWorkoutPlan.js

--- myWorkoutPlan.js
+++ myWorkoutPlan.js
@@ -7,7 +7,7 @@ var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 3000);
+app.set('port', 2500);
 app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
